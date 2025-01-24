96707da0a51306519f866e6786de836463f2138c
Noah Josey git config --global user.name Noah <dpk5yt@virginia.edu>
2025-01-24 10:25:41 -0500

    lab2

---
+++ Score_check.R
@@ -0,0 +1,15 @@
+score_check <- function(fp) {
+  # fp goes 0-120
+  #  under 100 = in the field (no score change)
+  #  100-110= TD
+  #  110-120= FG
+
+  if (fp > 100 && fp <= 110) {
+    return(7)
+  } else if (fp > 110 && fp <= 120) {
+    return(3)
+  } else {
+    # No score
+    return(NA)
+  }
+}
---
+++ sim_drive.R
@@ -0,0 +1,12 @@
+simulate_drive <- function(down, ytg, fp) {
+ 
+  # Random fp
+  new_fp <- runif(1, min = 0, max = 120)
+  
+  #starting new set of downs 
+  new_down <- 1
+  new_ytg  <- 10
+  
+  # Return new states 
+  return(list(down = new_down, ytg = new_ytg, fp = new_fp))
+}
