fef0da5358df1cbe2e5079a82ddd398b3a5ac405
Noah Josey git config --global user.name Noah <dpk5yt@virginia.edu>
2025-02-24 10:12:39 -0500

    lab5

--- run_play.R
+++ run_play.R
@@ -1,3 +1,40 @@
+# logistic regression to estimate FG success 
+predict_fg_success <- function(FP) {
+  # Ex. formula:
+  plogis(-5 + 0.08 * FP)
+}
+
+#multinomial regression to estimate the probabilities
+
+predict_4th_down_decision <- function(FP, YTG) {
+  
+  if (FP < 50) {
+    go_prob <- 0.1
+    fg_prob <- 0.1
+    punt_prob <- 0.8
+  } else if (FP < 70) {
+    go_prob <- 0.3
+    fg_prob <- 0.2
+    punt_prob <- 0.5
+  } else {
+    go_prob <- 0.2
+    fg_prob <- 0.5
+    punt_prob <- 0.3
+  }
+  
+  # Oadjust for large YTG
+  if (YTG > 10) {
+    go_prob <- go_prob * 0.5
+    # fg_prob stays the same
+    # punt_prob stays the same
+  }
+  
+  total <- go_prob + fg_prob + punt_prob
+  c(go_for_it = go_prob / total,
+    fg        = fg_prob / total,
+    punt      = punt_prob / total)
+}
+
 run_play <- function(D, YTG, FP) {
   if (D == 1) {
     out <- down_one(D, YTG, FP)
@@ -14,6 +51,7 @@ run_play <- function(D, YTG, FP) {
 # 1st down 
 down_one <- function(D, YTG, FP) {
   
+  # Sample yards gained
   yards_gained <- sample(-5:30, size = 1)
   
   # 2% chance of turnover
@@ -23,8 +61,12 @@ down_one <- function(D, YTG, FP) {
   new_FP <- FP + yards_gained
   is_touchdown <- (new_FP >= 100)
   
-  # exit drive for turnover 
-  if (forced_turnover) {
+  # check for safety
+  if (new_FP <= 0) {
+    # if safety, exit drive
+    list(D = D, YTG = YTG, FP = -5, exit_drive = 1)
+  } else if (forced_turnover) {
+    # exit drive for turnover
     list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
   } else if (is_touchdown) {
     # If touchdown set FP=105 as a marker
@@ -50,8 +92,11 @@ down_two <- function(D, YTG, FP) {
   new_FP <- FP + yards_gained
   is_touchdown <- (new_FP >= 100)
   
-  # turnover
-  if (forced_turnover) {
+  # check for safety
+  if (new_FP <= 0) {
+    list(D = D, YTG = YTG, FP = -5, exit_drive = 1)
+  } else if (forced_turnover) {
+    # turnover
     list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
   } else if (is_touchdown) {
     # Td scenario
@@ -76,7 +121,10 @@ down_three <- function(D, YTG, FP) {
   new_FP <- FP + yards_gained
   is_touchdown <- (new_FP >= 100)
   
-  if (forced_turnover) {
+  # check for safety
+  if (new_FP <= 0) {
+    list(D = D, YTG = YTG, FP = -5, exit_drive = 1)
+  } else if (forced_turnover) {
     list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
   } else if (is_touchdown) {
     list(D = D, YTG = YTG, FP = 105, exit_drive = 1)
@@ -94,16 +142,23 @@ down_three <- function(D, YTG, FP) {
 # 4th Down
 down_four <- function(D, YTG, FP) {
   
-  if (YTG < 2) {
+  decision_probs <- predict_4th_down_decision(FP, YTG)
+  decision <- sample(c("go_for_it", "fg", "punt"), size = 1, prob = decision_probs)
+  
+  if (decision == "go_for_it") {
     # go for it if under 2 yds, see if attempt is successful
+    # (We keep the same yardage distribution from your original code)
     yards_gained <- sample(-5:10, size = 1)  
     new_FP <- FP + yards_gained
     
+    # 2% chance of forced turnover (as in original go-for-it code)
     forced_turnover <- runif(1) < 0.02
     is_touchdown <- (new_FP >= 100)
     
-    # check for turnover
-    if (forced_turnover) {
+    # check for safety
+    if (new_FP <= 0) {
+      list(D = D, YTG = YTG, FP = -5, exit_drive = 1)
+    } else if (forced_turnover) {
       list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
     } else if (is_touchdown) {
       list(D = D, YTG = YTG, FP = 105, exit_drive = 1)
@@ -118,10 +173,10 @@ down_four <- function(D, YTG, FP) {
       }
     }
     
-  } else if (FP > 70) {
+  } else if (decision == "fg") {
     # fg attempt 
-    # 55% rate for fg being good
-    made_fg <- runif(1) < 0.55
+    p_fg <- predict_fg_success(FP)
+    made_fg <- runif(1) < p_fg
     if (made_fg) {
       # good fg = fp of 115
       list(D = D, YTG = YTG, FP = 115, exit_drive = 1)
@@ -129,18 +184,6 @@ down_four <- function(D, YTG, FP) {
       # Missed FG otherwise, turnover 
       list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
     }
-    
-  } else if (FP > 80) {
-    # fg attempt 
-    # 65% rate for fg being good
-    made_fg <- runif(1) < 0.65
-    if (made_fg) {
-      # good fg = fp of 115
-      list(D = D, YTG = YTG, FP = 115, exit_drive = 1)
-    } else {
-      # Missed FG otherwise, turnover 
-      list(D = D, YTG = YTG, FP = FP, exit_drive = 1)
-    }    
     
   } else {
     # Punt
